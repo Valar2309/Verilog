19aadb1aae33928a07aa51da01eca6474aea317f
Valar2309 <valarmathibaskar23@gmail.com>
2025-06-30 16:40:00 +0530

    Create Adder3.v

---
+++ HDLBits/Circuits/Combinational Logic/Arithmetic Circuits/Adder3.v
@@ -0,0 +1,14 @@
+module fadd(input a,b,cin,output sum,cout);
+    assign sum=a^b^cin;
+    assign cout=(a&b)|(b&cin)|(a&cin);
+endmodule    
+    
+module top_module( 
+    input [2:0] a, b,
+    input cin,
+    output [2:0] cout,
+    output [2:0] sum );
+    fadd inst1(a[0],b[0],cin,sum[0],cout[0]);
+    fadd inst2(a[1],b[1],cout[0],sum[1],cout[1]);
+    fadd inst3(a[2],b[2],cout[1],sum[2],cout[2]);
+endmodule
